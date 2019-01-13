c66cca092b032958183d3afe2791e5c3a47c25fa
Kyle Cooley <kyleacooley@gmail.com>
2019-01-13 17:09:38 -0500

    Setup travis

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# Build images
+docker build -t kylecooley/fib-client:latest -t kylecooley/fib-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kylecooley/fib-server:latest -t kylecooley/fib-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kylecooley/fib-worker:latest -t kylecooley/fib-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kylecooley/fib-client:latest
+docker push kylecooley/fib-server:latest
+docker push kylecooley/fib-worker:latest
+
+docker push kylecooley/fib-client:$SHA
+docker push kylecooley/fib-server:$SHA
+docker push kylecooley/fib-worker:$SHA
+
+# Deploy to k8s cluster
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=kylecooley/fib-client:$SHA
+kubectl set image deployments/server-deployment server=kylecooley/fib-server:$SHA
+kubectl set image deployments/worker-deployment worker=kylecooley/fib-worker:$SHA
